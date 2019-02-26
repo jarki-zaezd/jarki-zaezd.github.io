b689b5dbc64751d40714ca4d2de4529675a69510
RomanBorr <starmordar@gmail.com>
2019-02-26 18:45:38 +0300

    add calendar interaction

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,11 +1,12 @@
 let calendarConfig = {
     inline: true,
-    onMonthChange: function () {
+    onChange: function () {
         $('tbody').remove();
         //отправить в очередь, что бы сначало 
         //проинициализировалась таблциа, а после о,новилась статистика
-        setTimeout(createTable, 0)
+        setTimeout(updateCalendar, 0)
     }
+
 }
 
 flatpickr(".mini-calendar", calendarConfig);
@@ -16,8 +17,8 @@ function getDays() {
     let days = Array.from(document.querySelectorAll('.dayContainer span'));
     let daysCurrentMonth = [], daysPrevMonth = [], daysNextMonth = [];
     days.forEach(elem => {
-        if (elem.className === 'flatpickr-day prevMonthDay') daysPrevMonth.push(elem.textContent);
-        else if (elem.className === 'flatpickr-day nextMonthDay') daysNextMonth.push(elem.textContent);
+        if (elem.className.includes('flatpickr-day prevMonthDay')) daysPrevMonth.push(elem.textContent);
+        else if (elem.className.includes('flatpickr-day nextMonthDay')) daysNextMonth.push(elem.textContent);
         else daysCurrentMonth.push(elem.textContent);
     });
 
@@ -31,10 +32,55 @@ function getDays() {
     for (let i = 0; i < daysNextMonth.length; i++) {
         $thList.push("<th style='background: #edeff1;'> <div class='date'>" + daysNextMonth[i] + "</div></th>");
     }
+    console.log($thList);
     return $thList;
 }
 
-function createTable() {
+function setTitleToTableNav() {
+    let month = document.querySelector('.cur-month').textContent,
+        year = document.querySelector('.numInputWrapper input').value,
+        calendarNav = document.querySelector('.calendar-nav h4');
+
+    calendarNav.textContent = month + year;
+}
+
+function setPrevAndNextMonthToTableNav() {
+    let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let dateInfo = document.querySelector('.calendar-nav h4').textContent,
+        currentMonth = dateInfo.substr(0, dateInfo.indexOf(' ')),
+        numberMonth = month.indexOf(currentMonth),
+        year = dateInfo.slice(dateInfo.indexOf(' ')),
+        prev, next,
+        prevElem = document.querySelector('.calendar-nav .prev'),
+        nextElem = document.querySelector('.calendar-nav .next'),
+        prevIcon = '<i class="fa fa-chevron-left"></i>',
+        nextIcon = '<i class="fa fa-chevron-right"></i>';
+
+    if (numberMonth === 0) {
+        prev = month[11];
+        next = month[1];
+        nextElem.innerHTML = next + ' ' + year + nextIcon;
+        prevElem.innerHTML = prevIcon + prev + ' ' + --year;
+    }
+    else if (numberMonth === 11) {
+        prev = month[10];
+        next = month[0];
+        prevElem.innerHTML = prevIcon + prev + ' ' + year;
+        nextElem.innerHTML = next + ' ' + ++year + nextIcon;
+    }
+    else {
+        prev = month[numberMonth - 1];
+        next = month[numberMonth + 1];
+        prevElem.innerHTML = prevIcon + prev + ' ' + year;
+        nextElem.innerHTML = next + ' ' + year + nextIcon;
+    }
+}
+
+
+function updateCalendar() {
+
+    setTitleToTableNav();
+    setPrevAndNextMonthToTableNav();
     let $tblBody = $("<tbody></tbody>"), $tr;
     $thList = getDays();
     for (let i = 0; i < $thList.length; i++) {
@@ -42,8 +88,38 @@ function createTable() {
         $tr.append($thList[i]);
     }
     $('table').append($tblBody);
-
 }
 
-createTable();
+updateCalendar();
 
+
+$('.prev').click(changeDate);
+
+$('.next').click(changeDate);
+
+function changeDate(e) {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let numberMonths = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+
+    let tempMonth = this.textContent.substr(0, this.textContent.indexOf(' ')),
+        tempYear = this.textContent.slice(this.textContent.indexOf(' ') + 1);
+
+    let numberMonth = numberMonths[months.indexOf(tempMonth)];
+    let day = '01';
+
+    let date = [tempYear, numberMonth, day].join('-');
+
+    let calendarConfg = {
+        inline: true,
+        defaultDate: date,
+        onChange: function () {
+            $('tbody').remove();
+            setTimeout(updateCalendar, 0)
+        }
+    }
+
+    flatpickr(".mini-calendar", calendarConfg);
+    $('tbody').remove();
+    updateCalendar();
+    e.preventDefault();
+}
