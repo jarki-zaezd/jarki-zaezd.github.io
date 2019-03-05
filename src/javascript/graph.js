77dd15dd19f60aa3f1fd05c285aef79a455afbd3
RomanBorr <starmordar@gmail.com>
2019-03-05 18:31:18 +0300

    add const in graphs

--- src/javascript/graph.js
+++ src/javascript/graph.js
@@ -2,58 +2,88 @@ var randomScalingFactor = function () {
     return Math.round(Math.random() * 100);
 };
 
+const TRIP_LABELS = ["Pussy trip", "Savage trip", "Try escape trip", "Train trip", "No road trip", "Speed hack trip"];
+const MONTH_LIST = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-let ctx = document.getElementById('time').getContext('2d');
-var myPieChart = new Chart(ctx, {
+const DATA_FOR_TIME_GRAPH = [
+    randomScalingFactor(),
+    randomScalingFactor(),
+    randomScalingFactor(),
+    randomScalingFactor(),
+    randomScalingFactor(),
+    randomScalingFactor()
+];
+const DATA_FOR_DIFFICULTY_GRAPH = [
+    randomScalingFactor() * 100,
+    randomScalingFactor() * 100,
+    randomScalingFactor() * 100,
+    randomScalingFactor() * 100,
+    randomScalingFactor() * 100,
+    randomScalingFactor() * 100
+];
+const DATA_FOR_MILE_COUNT_GRAPH = [
+    randomScalingFactor() * 30,
+    randomScalingFactor() * 30,
+    randomScalingFactor() * 30,
+    randomScalingFactor() * 30,
+    randomScalingFactor() * 30,
+    randomScalingFactor() * 30
+];
+const DATA_FOR_SEASON_GRAPH = [
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50,
+    randomScalingFactor() * 2 + 50
+];
+
+const GREEN = 'rgb(183, 255, 181)';
+const WHITE = 'rgb(255, 255, 255)';
+const BLUE = 'rgb(136, 189, 243)';
+const ORANGE = 'rgb(245, 207, 143)';
+const NAVY_BLUE = 'rgb(66, 72, 235)';
+
+const COLORS_FOR_TIME_GRAPH = [GREEN, GREEN, GREEN, GREEN, GREEN, GREEN];
+const COLORS_FOR_DIFFICULTY_GRAPH = [BLUE, BLUE, BLUE, BLUE, BLUE, BLUE];
+const COLORS_FOR_MILE_COUNT_GRAPH = [ORANGE, ORANGE, ORANGE, ORANGE, ORANGE, ORANGE];
+
+let ctxForTimeCharter = document.getElementById('time').getContext('2d');
+var timeGraph = new Chart(ctxForTimeCharter, {
     type: 'bar',
     data: {
         datasets: [{
-            label: "Time spend",
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-            backgroundColor: [
-                'rgb(183, 255, 181)',
-                'rgb(183, 255, 181)',
-                'rgb(183, 255, 181)',
-                'rgb(183, 255, 181)',
-                'rgb(183, 255, 181)',
-                'rgb(183, 255, 181)'
-            ]
+            label: "Hours spent in trips",
+            data: DATA_FOR_TIME_GRAPH,
+            backgroundColor: COLORS_FOR_TIME_GRAPH
         }],
-        labels: [
-            "Pussy trip",
-            "Savage trip",
-            "Try escape trip",
-            "Train trip",
-            "No road trip",
-            "Speed hack trip"
-        ]
+        labels: TRIP_LABELS
     },
-
     options: {
         responsive: true,
         legend: {
             labels: {
-                fontColor: 'rgb(255, 255, 255)'
+                fontColor: WHITE
             }
         },
         scales: {
             yAxes: [{
                 display: true,
                 gridLines: {
-                    color: "#FFFFFF"
+                    color: WHITE
                 },
                 ticks: {
                     min: 0,
+                    max: 120,
                     stepSize: 20,
-                    fontColor: "white",
-                    callback: function (label, index, labels) {
+                    fontColor: WHITE,
+                    callback: function (label) {
                         return label + ' hours';
                     }
                 }
@@ -61,132 +91,93 @@ var myPieChart = new Chart(ctx, {
             xAxes: [{
                 gridLines: {
                     display: false,
-                    color: "#FFFFFF"
+                    color: WHITE
                 },
                 ticks: {
-                    fontColor: "white"
+                    fontColor: WHITE
                 }
             }]
         }
     }
 });
 
-let ctx2 = document.getElementById('trip-difficulty').getContext('2d');
-var myPieChart2 = new Chart(ctx2, {
+let ctxForDifficultyCharter = document.getElementById('trip-difficulty').getContext('2d');
+var difficultyGraph = new Chart(ctxForDifficultyCharter, {
     type: 'bar',
     data: {
         datasets: [{
-            label: "Difficulty",
-            data: [
-                randomScalingFactor() * 100,
-                randomScalingFactor() * 100,
-                randomScalingFactor() * 100,
-                randomScalingFactor() * 100,
-                randomScalingFactor() * 100,
-                randomScalingFactor() * 100
-            ],
-            backgroundColor: [
-                'rgb(136, 189, 243)',
-                'rgb(136, 189, 243)',
-                'rgb(136, 189, 243)',
-                'rgb(136, 189, 243)',
-                'rgb(136, 189, 243)',
-                'rgb(136, 189, 243)'
-            ]
+            label: "How many times did",
+            data: DATA_FOR_DIFFICULTY_GRAPH,
+            backgroundColor: COLORS_FOR_DIFFICULTY_GRAPH
         }],
-        labels: [
-            "Pussy trip",
-            "Savage trip",
-            "Try escape trip",
-            "Train trip",
-            "No road trip",
-            "Speed hack trip"
-        ]
+        labels: TRIP_LABELS
     },
 
     options: {
         responsive: true,
         legend: {
             labels: {
-                fontColor: 'rgb(255, 255, 255)'
+                fontColor: WHITE
             }
         },
         scales: {
             yAxes: [{
                 display: true,
                 gridLines: {
-                    color: "#FFFFFF"
+                    color: WHITE
                 },
                 ticks: {
                     min: 0,
+                    max: 12000,
                     stepSize: 2000,
-                    fontColor: "white"
+                    fontColor: WHITE
                 }
             }],
             xAxes: [{
+                display: true,
                 gridLines: {
                     display: false,
-                    color: "#FFFFFF"
+                    color: WHITE
                 },
                 ticks: {
-                    fontColor: "white"
+                    fontColor: WHITE
                 }
             }]
         }
     }
 });
 
-let ctx3 = document.getElementById('trip-all-way').getContext('2d');
-var myPieChart3 = new Chart(ctx3, {
+let ctxForMileCountCharter = document.getElementById('trip-all-way').getContext('2d');
+var mileCountGraph = new Chart(ctxForMileCountCharter, {
     type: 'bar',
     data: {
         datasets: [{
-            label: "Mile count",
-            data: [
-                randomScalingFactor() * 30,
-                randomScalingFactor() * 30,
-                randomScalingFactor() * 30,
-                randomScalingFactor() * 30,
-                randomScalingFactor() * 30,
-                randomScalingFactor() * 30
-            ],
-            backgroundColor: [
-                'rgb(245, 207, 143)',
-                'rgb(245, 207, 143)',
-                'rgb(245, 207, 143)',
-                'rgb(245, 207, 143)',
-                'rgb(245, 207, 143)',
-                'rgb(245, 207, 143)'
-            ]
+            label: "How many miles",
+            data: DATA_FOR_MILE_COUNT_GRAPH,
+            backgroundColor: COLORS_FOR_MILE_COUNT_GRAPH
         }],
-        labels: [
-            "Pussy trip",
-            "Savage trip",
-            "Try escape trip",
-            "Train trip",
-            "No road trip",
-            "Speed hack trip"
-        ]
+        labels: TRIP_LABELS
     },
 
     options: {
         responsive: true,
         legend: {
             labels: {
-                fontColor: 'rgb(255, 255, 255)'
+                fontColor: WHITE
             }
         },
         scales: {
             yAxes: [{
                 display: true,
                 gridLines: {
-                    color: "#FFFFFF"
+                    color: WHITE
                 },
                 ticks: {
                     min: 0,
+                    max: 3500,
                     stepSize: 700,
-                    fontColor: "white",
-                    callback: function (label, index, labels) {
+                    fontColor: WHITE,
+                    callback: function (label) {
                         return label + ' mile';
                     }
                 }
@@ -194,40 +185,27 @@ var myPieChart3 = new Chart(ctx3, {
             xAxes: [{
                 gridLines: {
                     display: false,
-                    color: "#FFFFFF"
+                    color: WHITE
                 },
                 ticks: {
 
-                    fontColor: "white"
+                    fontColor: WHITE
                 }
             }]
         }
     }
 });
 
-let ctx4 = document.getElementById('trip-activity').getContext('2d');
-var myPieChart4 = new Chart(ctx4, {
+let ctxForSeasonCharter = document.getElementById('trip-activity').getContext('2d');
+var seasonGraph = new Chart(ctxForSeasonCharter, {
     type: 'line',
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        labels: MONTH_LIST,
         datasets: [{
-            label: "Trip activity",
-            backgroundColor: "rgb(66, 72, 235)",
-            borderColor: "rgb(66, 72, 235)",
-            data: [
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150,
-                randomScalingFactor() + 150
-            ],
+            label: "Seasonal activities",
+            backgroundColor: NAVY_BLUE,
+            borderColor: NAVY_BLUE,
+            data: DATA_FOR_SEASON_GRAPH,
             fill: false
         }],
     },
@@ -236,27 +214,29 @@ var myPieChart4 = new Chart(ctx4, {
         responsive: true,
         legend: {
             labels: {
-                fontColor: 'rgb(255, 255, 255)'
+                fontColor: WHITE
             }
         },
         scales: {
             yAxes: [{
                 display: true,
                 gridLines: {
-                    color: "#FFFFFF"
+                    color: WHITE
                 },
                 ticks: {
-                    stepSize: 30,
-                    fontColor: "white"
+                    min: 0,
+                    max: 280,
+                    stepSize: 70,
+                    fontColor: WHITE
                 }
             }],
             xAxes: [{
                 gridLines: {
                     display: false,
-                    color: "#FFFFFF"
+                    color: WHITE
                 },
                 ticks: {
-                    fontColor: "white"
+                    fontColor: WHITE
                 }
             }]
         }
