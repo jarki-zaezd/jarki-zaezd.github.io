4c4894fca78589d0fda17c09779328a655dbfd06
RomanBorr <starmordar@gmail.com>
2019-03-08 21:36:01 +0300

    add class Table

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,100 +1,101 @@
+class Table {
+    setTitleToTableNav() {
+        let month = document.querySelector('.cur-month').textContent,
+            year = document.querySelector('.numInputWrapper input').value,
+            calendarNav = document.querySelector('.calendar-nav h4');
+
+        calendarNav.textContent = month + year;
+    }
+
+    setPrevAndNextMonthToTableNav() {
+        let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        let dateInfo = document.querySelector('.calendar-nav h4').textContent,
+            currentMonth = dateInfo.substr(0, dateInfo.indexOf(' ')),
+            numberMonth = month.indexOf(currentMonth),
+            year = dateInfo.slice(dateInfo.indexOf(' ')),
+            prev, next,
+            prevElem = document.querySelector('.calendar-nav .prev'),
+            nextElem = document.querySelector('.calendar-nav .next'),
+            prevIcon = '<i class="fa fa-chevron-left"></i>',
+            nextIcon = '<i class="fa fa-chevron-right"></i>';
+
+        if (numberMonth === 0) {
+            prev = month[11];
+            next = month[1];
+            nextElem.innerHTML = next + ' ' + year + nextIcon;
+            prevElem.innerHTML = prevIcon + prev + ' ' + --year;
+        }
+        else if (numberMonth === 11) {
+            prev = month[10];
+            next = month[0];
+            prevElem.innerHTML = prevIcon + prev + ' ' + year;
+            nextElem.innerHTML = next + ' ' + ++year + nextIcon;
+        }
+        else {
+            prev = month[numberMonth - 1];
+            next = month[numberMonth + 1];
+            prevElem.innerHTML = prevIcon + prev + ' ' + year;
+            nextElem.innerHTML = next + ' ' + year + nextIcon;
+        }
+    }
+
+    getDays() {
+        let days = Array.from(document.querySelectorAll('.dayContainer span'));
+        let daysCurrentMonth = [], daysPrevMonth = [], daysNextMonth = [];
+        days.forEach(elem => {
+            if (elem.className.includes('flatpickr-day prevMonthDay')) daysPrevMonth.push(elem.textContent);
+            else if (elem.className.includes('flatpickr-day nextMonthDay')) daysNextMonth.push(elem.textContent);
+            else daysCurrentMonth.push(elem.textContent);
+        });
+
+        let $thList = [];
+        for (let i = 0; i < daysPrevMonth.length; i++) {
+            $thList.push("<th style='background: #edeff1;'> <div class='date'>" + daysPrevMonth[i] + "</div></th>");
+        }
+        for (let i = 0; i < daysCurrentMonth.length; i++) {
+            $thList.push("<th> <div class='date'>" + daysCurrentMonth[i] + "</div></th>");
+        }
+        for (let i = 0; i < daysNextMonth.length; i++) {
+            $thList.push("<th style='background: #edeff1;'> <div class='date'>" + daysNextMonth[i] + "</div></th>");
+        }
+        return $thList;
+    }
+
+    updateCalendar() {
+        this.setTitleToTableNav();
+        this.setPrevAndNextMonthToTableNav();
+        let $tblBody = $("<tbody></tbody>"), $tr;
+        let $thList = this.getDays();
+        for (let i = 0; i < $thList.length; i++) {
+            if ($tr === undefined || $tr.children().length === 7) $tr = $("<tr/>").appendTo($tblBody);
+            $tr.append($thList[i]);
+        }
+        $('table').append($tblBody);
+    }
+}
+
+let tbl = new Table();
+
 let calendarConfig = {
     inline: true,
     onChange: function () {
         $('tbody').remove();
         //отправить в очередь, что бы сначало 
         //проинициализировалась таблциа, а после о,новилась статистика
-        setTimeout(updateCalendar, 0)
+        setTimeout(tbl.updateCalendar(), 0)
     }
 
 }
 
 flatpickr(".mini-calendar", calendarConfig);
+tbl.updateCalendar();
 
-
-
-function getDays() {
-    let days = Array.from(document.querySelectorAll('.dayContainer span'));
-    let daysCurrentMonth = [], daysPrevMonth = [], daysNextMonth = [];
-    days.forEach(elem => {
-        if (elem.className.includes('flatpickr-day prevMonthDay')) daysPrevMonth.push(elem.textContent);
-        else if (elem.className.includes('flatpickr-day nextMonthDay')) daysNextMonth.push(elem.textContent);
-        else daysCurrentMonth.push(elem.textContent);
-    });
-
-    let $thList = [];
-    for (let i = 0; i < daysPrevMonth.length; i++) {
-        $thList.push("<th style='background: #edeff1;'> <div class='date'>" + daysPrevMonth[i] + "</div></th>");
-    }
-    for (let i = 0; i < daysCurrentMonth.length; i++) {
-        $thList.push("<th> <div class='date'>" + daysCurrentMonth[i] + "</div></th>");
-    }
-    for (let i = 0; i < daysNextMonth.length; i++) {
-        $thList.push("<th style='background: #edeff1;'> <div class='date'>" + daysNextMonth[i] + "</div></th>");
-    }
-    return $thList;
-}
-
-function setTitleToTableNav() {
-    let month = document.querySelector('.cur-month').textContent,
-        year = document.querySelector('.numInputWrapper input').value,
-        calendarNav = document.querySelector('.calendar-nav h4');
-
-    calendarNav.textContent = month + year;
-}
-
-function setPrevAndNextMonthToTableNav() {
-    let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let dateInfo = document.querySelector('.calendar-nav h4').textContent,
-        currentMonth = dateInfo.substr(0, dateInfo.indexOf(' ')),
-        numberMonth = month.indexOf(currentMonth),
-        year = dateInfo.slice(dateInfo.indexOf(' ')),
-        prev, next,
-        prevElem = document.querySelector('.calendar-nav .prev'),
-        nextElem = document.querySelector('.calendar-nav .next'),
-        prevIcon = '<i class="fa fa-chevron-left"></i>',
-        nextIcon = '<i class="fa fa-chevron-right"></i>';
-
-    if (numberMonth === 0) {
-        prev = month[11];
-        next = month[1];
-        nextElem.innerHTML = next + ' ' + year + nextIcon;
-        prevElem.innerHTML = prevIcon + prev + ' ' + --year;
-    }
-    else if (numberMonth === 11) {
-        prev = month[10];
-        next = month[0];
-        prevElem.innerHTML = prevIcon + prev + ' ' + year;
-        nextElem.innerHTML = next + ' ' + ++year + nextIcon;
-    }
-    else {
-        prev = month[numberMonth - 1];
-        next = month[numberMonth + 1];
-        prevElem.innerHTML = prevIcon + prev + ' ' + year;
-        nextElem.innerHTML = next + ' ' + year + nextIcon;
-    }
-}
-
-
-function updateCalendar() {
-
-    setTitleToTableNav();
-    setPrevAndNextMonthToTableNav();
-    let $tblBody = $("<tbody></tbody>"), $tr;
-    $thList = getDays();
-    for (let i = 0; i < $thList.length; i++) {
-        if ($tr === undefined || $tr.children().length === 7) $tr = $("<tr/>").appendTo($tblBody);
-        $tr.append($thList[i]);
-    }
-    $('table').append($tblBody);
-}
-
-updateCalendar();
 
 
 $('.prev').click(changeDate);
 
 $('.next').click(changeDate);
+
 
 function changeDate(e) {
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -113,13 +114,14 @@ function changeDate(e) {
         defaultDate: date,
         onChange: function () {
             $('tbody').remove();
-            setTimeout(updateCalendar, 0)
+            setTimeout(tbl.updateCalendar(), 0)
         }
     }
 
     flatpickr(".mini-calendar", calendarConfg);
     $('tbody').remove();
-    updateCalendar();
+    console.log(tbl);
+    tbl.updateCalendar();
     e.preventDefault();
 }
 
