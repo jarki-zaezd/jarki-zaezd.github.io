06f004330e56d82fb4582149476c25f9d54f4927
RomanBorr <starmordar@gmail.com>
2019-03-14 20:49:28 +0300

    add weather icons

--- src/javascript/scrap.js
+++ src/javascript/scrap.js
@@ -27,6 +27,51 @@ $.getJSON('https://whateverorigin.herokuapp.com/get?url=' +
             weatherIcon.push(key);
 
         }
+        class WeatherItem {
+            constructor(min, max, status, day) {
+                this.min = parseInt(min);
+                this.max = parseInt(max);
+                this.status = status;
+                this.day = parseInt(day);
+            }
+        }
 
-        console.log(weatherIcon);
+        let today = new Date();
+        let today2 = today.getDate();
+        let index = daysNumbers.indexOf(String(today2));
+
+        let weatherObjs = [];
+        for (let i = 0; i < weatherMinNumbers.length; i++) {
+            let tmp = new WeatherItem(weatherMinNumbers[i], weatherMaxNumbers[i], weatherStat[i], daysNumbers[i]);
+            if (tmp.status === undefined) continue;
+            weatherObjs.push(tmp);
+        }
+
+        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        let numberMonths = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+        let monthNumber = today.getMonth();
+
+        let $th = $('tbody th');
+        let savePoint = 0;
+
+        for (let i = 0; i < $th.length; i++) {
+            if (weatherObjs[0].day == $th[i].textContent) {
+                savePoint = i;
+                break;
+            }
+        }
+
+        let curMonth = months[Number(monthNumber)];
+        let nextMonth = months[Number(monthNumber) + 1]
+
+        let calendarTitle = $('.calendar-nav h4').text();
+
+        if (calendarTitle.includes(curMonth)) {
+            for (let i = 0; i < weatherObjs.length; i++) {
+                $($th[i + savePoint]).append("<p>" + weatherObjs[i].max + "</p>");
+                $($th[i + savePoint]).append("<p>" + weatherObjs[i].min + "</p>");
+                $($th[i + savePoint]).append("<i class=\"wi " + "wi-night-sleet" + "\"></i>");
+            }
+        }
+
     });
