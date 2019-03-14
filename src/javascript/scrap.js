ab924cadec9b1290e90c4552c14edbb6562e3d36
RomanBorr <starmordar@gmail.com>
2019-03-14 11:45:41 +0300

    add weather icon map

--- src/javascript/scrap.js
+++ src/javascript/scrap.js
@@ -1,3 +1,22 @@
+const weatherHelper = {
+    "Пасмурно, осадки": "wi-day-rain-wind",
+    "Пасмурно": "wi-cloud",
+    "Облачно, небольшой дождь": "wi-hail",
+    "Облачно": "wi-cloudy",
+    "Пасмурно, дождь": "wi-hail",
+    "Переменная облачность, небольшой дождь": "wi-rain",
+    "Пасмурно, небольшой дождь": "wi-rain",
+    "Переменная облачность, небольшой снег": "wi-snow",
+    "Переменная облачность": "wi-cloudy",
+    "Облачно, дождь": "wi-rain",
+    "Малооблачно": "wi-cloud",
+    "Ясно": "wi-day-sunny",
+    "Малооблачно, небольшой дождь": "wi-rain",
+    "Малооблачно, дождь": "wi-rain"
+}
+
+
+
 $.getJSON('http://www.whateverorigin.org/get?url=' +
     encodeURIComponent('https://www.gismeteo.by/weather-minsk-4248/month/') + '&callback=?', function (data) {
         let weatherMaxData = $(data.contents).find('.temp_max.js_meas_container');
@@ -5,8 +24,8 @@ $.getJSON('http://www.whateverorigin.org/get?url=' +
         let days = $(data.contents).find('.date');
         let weatherStatus = $(data.contents).find('.tooltip.cell');
         let daysNumbers = [], weatherMaxNumbers = [], weatherMinNumbers = [], weatherStat = [];
-        $.each(weatherStatus, function (index, value) { 
-             weatherStat.push(value.dataset.text);
+        $.each(weatherStatus, function (index, value) {
+            weatherStat.push(value.dataset.text);
         });
         $.each(days, function (index, value) {
             let tmp = value.textContent.trim().slice(0, 2);
@@ -18,4 +37,15 @@ $.getJSON('http://www.whateverorigin.org/get?url=' +
         $.each(weatherMaxData, function (index, value) {
             weatherMaxNumbers.push(value.dataset.value);
         });
+        let weatherIcon = [];
+
+        for (let i = 0; i < weatherStat.length; i++) {
+            let key = weatherHelper[weatherStat[i]];
+            if (key === undefined) key = 'skip';
+
+            weatherIcon.push(key);
+
+        }
+
+        console.log(weatherIcon);
     });
